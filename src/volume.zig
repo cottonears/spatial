bb620545d2002bea2d18bd70bb4a2368e88f5a21
cottonears <cottonears@proton.me>
2025-01-21 12:12:25 +0000

    Working towards supporting heterogenous volume functions + making more user-friendly.

--- src/volume.zig
+++ src/volume.zig
@@ -2,94 +2,101 @@ const std = @import("std");
 const calc = @import("calc.zig");
 const Vec2f = calc.Vec2f;
 
-// TODO: Implement overlapsOther for balls + boxes.
-//       We can probably use anytype and do a comptime switch?
-
 /// A circular region in 2D Euclidean space.
-///  I.e., the region B := { b in R^2 : ||c, b|| < R }.
+/// B := { b in R^2 : ||c, b|| < radius }.
 pub const Ball2f = struct {
     centre: Vec2f,
-    radius: f32 = 0,
-    const Self = @This();
-
-    /// Returns a ball that encompasses both input balls.
-    pub fn getEncompassing(a: Ball2f, b: Ball2f) Ball2f {
-        const d = a.centre - b.centre;
-        const dist = calc.norm(d);
-        if (b.radius == 0 or (dist + b.radius < a.radius)) return a; // a encompasses b
-        if (a.radius == 0 or (dist + a.radius < b.radius)) return b; // b encompasses a
-        // otherwise, create a new circle that encompasses both a and b
-        return .{
-            .radius = 0.5 * (dist + a.radius + b.radius),
-            .centre = calc.scaledVec(0.5, (a.centre + b.centre)) + calc.scaledVec(0.5 * (a.radius - b.radius) / dist, d),
-        };
-    }
-
-    /// Returns true if this ball is empty.
-    pub fn isEmpty(self: Self) bool {
-        return self.radius == 0;
-    }
-
-    /// Returns true if the two balls overlap.
-    /// Assumes neither of them is empty.
-    pub fn overlapsOther(self: Self, other: Ball2f) bool {
-        const d = self.centre - other.centre;
-        const r_sum = self.radius + other.radius;
-        return calc.squaredSum(d) < r_sum * r_sum;
-    }
-
-    // One way of doing this would be to do the following (might be a bit inefficient):
-    // - define d as the line joining the centres
-    // - check where the box's edges intersect d
-    // - overlap if the intersection point is within the ball's radius of its centre
-    // pub fn overlapsBox(self: Self, b: Box2f) bool {
-    //     return false;
-    // }
+    radius: f32,
 };
 
 /// An axis-aligned rectangular region in 2D Euclidean space.
-///  I.e., the region B: = { b in R^2 : |c_y - b_y| < 0.5 * HW and |c_y - b_y| < 0.5 * HH }.
+///  B: = { b in R^2 : |c[0] - b[0]| < half_width and |c[1] - b[1]| < half_height }.
 pub const Box2f = struct {
     centre: Vec2f,
-    half_width: f32 = 0.0,
-    half_height: f32 = 0.0,
-    const Self = @This();
+    half_width: f32,
+    half_height: f32,
 
-    pub fn getEncompassing(a: Box2f, b: Box2f) Box2f {
-        if (b.half_width == 0 or b.half_height == 0) return a; // a encompasses b
-        if (a.half_width == 0 or a.half_height == 0) return b; // b encompasses a
-        const x_min = @min(a.centre[0] - a.half_width, b.centre[0] - b.half_width);
-        const x_max = @max(a.centre[0] + a.half_width, b.centre[0] + b.half_width);
-        const y_min = @min(a.centre[1] - a.half_height, b.centre[1] - b.half_height);
-        const y_max = @max(a.centre[1] + a.half_height, b.centre[1] + b.half_height);
-        const half_width = 0.5 * (x_max - x_min);
-        const half_height = 0.5 * (y_max - y_min);
-        return .{
-            .centre = Vec2f{ x_min + half_width, y_min + half_height },
-            .half_width = half_width,
-            .half_height = half_height,
-        };
-    }
-
-    /// Returns true if this box is empty.
-    pub fn isEmpty(self: Self) bool {
-        return self.half_width == 0 or self.half_height == 0;
-    }
-
-    /// Returns true if the two boxes overlap.
-    /// Assumes neither of them is empty.
-    pub fn overlapsOther(self: Self, other: Box2f) bool {
-        const d = self.centre - other.centre;
-        const width_sum = self.half_width + other.half_width;
-        const height_sum = self.half_height + other.half_height;
-        return @abs(d[0]) < width_sum and @abs(d[1]) < height_sum;
-    }
-
-    pub fn getCorners(self: Self) [2]Vec2f {
+    pub fn getCorners(self: Box2f) [2]Vec2f {
         const offset = Vec2f{ self.half_width, self.half_height };
         return [2]Vec2f{ self.centre - offset, self.centre + offset };
     }
 };
+
+// TODO: Investigate how performance is affected if volumes in overlap to be null.
+
+/// Returns true if the two balls overlap.
+/// Assumes neither of them is empty.
+pub fn checkOverlapBallBall(a: Ball2f, b: Ball2f) bool {
+    const vec_diff = a.centre - b.centre;
+    const r_sum = a.radius + b.radius;
+    return calc.squaredSum(vec_diff) < r_sum * r_sum;
+}
+
+/// Returns true if the two boxes overlap.
+/// Assumes neither of them is empty.
+pub fn checkOverlapBoxBox(a: Box2f, b: Box2f) bool {
+    const vec_diff = a.centre - b.centre;
+    const width_sum = a.half_width + b.half_width;
+    const height_sum = a.half_height + b.half_height;
+    return @abs(vec_diff[0]) < width_sum and @abs(vec_diff[1]) < height_sum;
+}
+
+/// Returns true if the ball and box overlap.
+/// Assumes neither of them is empty.
+pub fn checkOverlapBallBox(a: Ball2f, b: Box2f) bool {
+    // TODO: implement this!
+    _ = a;
+    _ = b;
+    return false;
+}
+
+/// Returns true if the two volumes overlap.
+/// Assumes neither of them is empty
+pub fn checkVolumesOverlap(a: anytype, b: anytype) bool {
+    return switch (@TypeOf(a)) {
+        Ball2f => if (@TypeOf(b) == Ball2f) checkOverlapBallBall(a, b) else checkOverlapBallBox(a, b),
+        Box2f => if (@TypeOf(b) == Box2f) checkOverlapBoxBox(a, b) else checkOverlapBallBox(b, a),
+        else => unreachable, // overlap check has not been implemented for this volume
+    };
+}
+
+/// Returns a ball that encompasses both input balls.
+pub fn getEncompassingBall(a: ?Ball2f, b: ?Ball2f) ?Ball2f {
+    if (a == null) return b;
+    if (b == null) return a;
+    const vec_sum = a.?.centre + b.?.centre;
+    const vec_diff = a.?.centre - b.?.centre;
+    const r_sum = a.?.radius + b.?.radius;
+    const r_diff = a.?.radius - b.?.radius;
+    const centre_dist = calc.norm(vec_diff);
+    const centre_offset = if (centre_dist > 0) calc.scaledVec(0.5 * r_diff / centre_dist, vec_diff) else Vec2f{ 0, 0 };
+    const enc_centre = calc.scaledVec(0.5, vec_sum) + centre_offset;
+    const enc_radius = 0.5 * (centre_dist + r_sum);
+    return .{ .centre = enc_centre, .radius = enc_radius };
+}
+
+/// Returns a box that encompasses both input boxes.
+pub fn getEncompassingBox(a: ?Box2f, b: ?Box2f) ?Box2f {
+    if (a == null) return b;
+    if (b == null) return a;
+    const x_min = @min(a.?.centre[0] - a.?.half_width, b.?.centre[0] - b.?.half_width);
+    const x_max = @max(a.?.centre[0] + a.?.half_width, b.?.centre[0] + b.?.half_width);
+    const y_min = @min(a.?.centre[1] - a.?.half_height, b.?.centre[1] - b.?.half_height);
+    const y_max = @max(a.?.centre[1] + a.?.half_height, b.?.centre[1] + b.?.half_height);
+    const half_width = 0.5 * (x_max - x_min);
+    const half_height = 0.5 * (y_max - y_min);
+    const enc_centre = Vec2f{ x_min + half_width, y_min + half_height };
+    return .{ .centre = enc_centre, .half_width = half_width, .half_height = half_height };
+}
+
+// TODO: make this more robust
+pub fn getEncompassingVolume(T: type, a: anytype, b: anytype) ?T {
+    return switch (T) {
+        Ball2f => getEncompassingBall(a, b),
+        Box2f => getEncompassingBox(a, b),
+        else => unreachable,
+    };
+}
 
 const testing = std.testing;
 
@@ -100,13 +107,13 @@ test "check balls overlap" {
     const b3 = Ball2f{ .centre = Vec2f{ 1.0, 1.0 }, .radius = 0.4 };
     const b4 = Ball2f{ .centre = Vec2f{ 1.5, 0.0 }, .radius = 0.5 };
 
-    const check_1 = a.overlapsOther(b1);
+    const check_1 = checkOverlapBallBall(a, b1);
     try testing.expectEqual(true, check_1);
-    const check_2 = a.overlapsOther(b2);
+    const check_2 = checkOverlapBallBall(a, b2);
     try testing.expectEqual(true, check_2);
-    const check_3 = a.overlapsOther(b3);
+    const check_3 = checkOverlapBallBall(a, b3);
     try testing.expectEqual(false, check_3);
-    const check_4 = a.overlapsOther(b4);
+    const check_4 = checkOverlapBallBall(a, b4);
     try testing.expectEqual(false, check_4);
 }
 
@@ -116,12 +123,16 @@ test "boxes overlap" {
     const b2 = Box2f{ .centre = Vec2f{ 0.625, 0.625 }, .half_width = 0.125, .half_height = 0.125 };
     const b3 = Box2f{ .centre = Vec2f{ 1.125, 1.125 }, .half_width = 0.375, .half_height = 0.375 };
     const b4 = Box2f{ .centre = Vec2f{ 1.625, 1.625 }, .half_width = 0.125, .half_height = 0.125 };
-    const check_1 = a.overlapsOther(b1);
-    const check_2 = a.overlapsOther(b2);
-    const check_3 = a.overlapsOther(b3);
-    const check_4 = a.overlapsOther(b4);
+    const check_1 = checkOverlapBoxBox(a, b1);
+    const check_2 = checkOverlapBoxBox(a, b2);
+    const check_3 = checkOverlapBoxBox(a, b3);
+    const check_4 = checkOverlapBoxBox(a, b4);
     try testing.expectEqual(true, check_1);
     try testing.expectEqual(true, check_2);
     try testing.expectEqual(true, check_3);
     try testing.expectEqual(false, check_4);
 }
+
+// TODO: write further encompassing + overlap tests
+// currently this case is bugged
+// 1. A = (c = { 0.029, 0.378 }, r = 0.199), B = (c = { 0.123, 0.355 }, r = 0.031);  C = (c = { -0.006, 0.387 }, r = 0.163)
