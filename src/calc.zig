bd8b6648f064e0e515a7fb650624d15a964d26cd
cottonears <cottonears@proton.me>
2025-02-01 11:50:28 +0000

    Corrected some errors in comments.

--- src/calc.zig
+++ src/calc.zig
@@ -1,8 +1,8 @@
 //! This module contains core maths functions used throughout the library.
-//! TODO: add support for doubles and higher-dimensional vectors
+
 const std = @import("std");
 pub const Vec2f = @Vector(2, f32);
-const zero_2f = Vec2f{ 0, 0 };
+pub const zero2f = Vec2f{ 0, 0 };
 
 /// Gets an array containing the range: 0, 1, ... len - 1.
 pub fn getRange(comptime T: type, comptime len: u24) [len]T {
@@ -28,23 +28,23 @@ pub fn getPow2nSequence(comptime base: u8, comptime n_start: u8, comptime n_end:
     return seq;
 }
 
-/// Converts an int to a 32 bit float. Saves a bit of typing.
-pub fn asf32(x: anytype) f32 {
-    return @floatFromInt(x);
+/// Converts the integer k to a 32 bit float. Saves a bit of typing.
+pub fn asf32(k: anytype) f32 {
+    return @floatFromInt(k);
 }
 
-/// Returns the Euclidean norm of the vector v.
+/// Returns || v ||, the Euclidean norm of the vector v.
 pub fn norm(v: anytype) f32 {
     return @sqrt(squaredSum(v));
 }
 
-/// Returns the norm (length) of the vector v.
+/// Returns < v, v >, the dot product of v with itself.
 pub fn squaredSum(v: anytype) f32 {
-    return innerProd(v, v);
+    return dotProduct(v, v);
 }
 
-/// Computes the inner product of two vectors of f32s.
-pub fn innerProd(a: anytype, b: anytype) f32 {
+/// Returns < a, b >, the dot product a and b.
+pub fn dotProduct(a: anytype, b: anytype) f32 {
     return @reduce(.Add, a * b);
 }
 
@@ -56,7 +56,7 @@ pub fn vecsEqual(a: anytype, b: anytype) bool {
 /// Returns a unit length vector in the same direction as v, or a zero vector if it has zero length.
 pub fn normalised(v: anytype) @TypeOf(v) {
     const n = norm(v);
-    return if (n > 0) scaledVec(1 / norm(v), v) else zero_2f;
+    return if (n > 0) scaledVec(1 / norm(v), v) else zero2f;
 }
 
 /// Returns a scaled version of the input vector.
@@ -67,33 +67,30 @@ pub fn scaledVec(alpha: f32, v: anytype) @TypeOf(v) {
 
 // TODO: check if this generalises to higher dimensions and modify parameters if it does.
 // solves for t that minimises || u + t * v ||
-pub fn solveForMinDist(u: Vec2f, v: Vec2f) f32 {
-    const denom = innerProd(v, v);
-    return if (denom == 0) 0 else -innerProd(u, v) / denom;
+fn solveTimeThatMinimisesDist(u: Vec2f, v: Vec2f) f32 {
+    const denom = dotProduct(v, v);
+    return if (denom == 0) 0 else -dotProduct(u, v) / denom;
 }
 
-// returns the minimum value of || u_a + t * v_a - (b_a - t * v_a) || ^ 2 with no restriction on t
-pub fn getMinDistSquared(pos_a: Vec2f, vel_a: Vec2f, pos_b: Vec2f, vel_b: Vec2f) f32 {
-    const u = pos_a - pos_b;
-    const v = vel_a - vel_b;
-    const t: f32 = solveForMinDist(u, v);
+/// Returns (t, d_min^2) between two points moving with constant velocity (no restriction on t).
+/// Each point is described by the equation: r(t) = u + t * v (u and v are vectors, t is a real number).
+pub fn solveMinDistSquared(u_a: Vec2f, v_a: Vec2f, u_b: Vec2f, v_b: Vec2f) Vec2f {
+    const u = u_a - u_b;
+    const v = v_a - v_b;
+    const t: f32 = solveTimeThatMinimisesDist(u, v);
     const d = u + scaledVec(t, v);
-    return squaredSum(d);
+    return .{ t, squaredSum(d) };
 }
 
-// same as the above but clamps range of t to [t_min, t_max]
-pub fn getMinDistSquaredForInterval(pos_a: Vec2f, vel_a: Vec2f, pos_b: Vec2f, vel_b: Vec2f, t_min: f32, t_max: f32) f32 {
-    const u = pos_a - pos_b;
-    const v = vel_a - vel_b;
-    var t = solveForMinDist(u, v);
+/// Returns (t, d_min^2) between two points moving with constant velocity; t clamped to [t_min, t_max].
+/// Each point is described by the equation: r(t) = u + t * v (u and v are vectors, t is a real number).
+pub fn solveMinDistSquaredClamp(u_a: Vec2f, v_a: Vec2f, u_b: Vec2f, v_b: Vec2f, t_min: f32, t_max: f32) Vec2f {
+    const u = u_a - u_b;
+    const v = v_a - v_b;
+    var t = solveTimeThatMinimisesDist(u, v);
     t = @max(t_min, @min(t, t_max));
     const d = u + scaledVec(t, v);
-    return squaredSum(d);
-}
-
-// NOTE: Doesn't handle case where half_width = 0 (implying the open ball is the empty set) sensibly.
-pub fn intervalsOverlap(centre_a: f32, half_width_a: f32, centre_b: f32, half_width_b: f32) bool {
-    return @abs(centre_a - centre_b) < half_width_a + half_width_b;
+    return .{ t, squaredSum(d) };
 }
 
 const testing = std.testing;
@@ -123,32 +120,32 @@ test "closest dist" {
     const pos_a = Vec2f{ 1, 0 };
     const pos_b = Vec2f{ 0, 1 };
 
-    const zero_result = getMinDistSquared(zero_2f, zero_2f, zero_2f, zero_2f);
-    try testing.expectApproxEqAbs(0, zero_result, tolerance);
+    const zero_result = solveMinDistSquared(zero2f, zero2f, zero2f, zero2f);
+    try testing.expectApproxEqAbs(0, zero_result[1], tolerance);
 
-    const static_result = getMinDistSquared(pos_a, zero_2f, pos_b, zero_2f);
-    try testing.expectApproxEqAbs(2, static_result, tolerance);
+    const static_result = solveMinDistSquared(pos_a, zero2f, pos_b, zero2f);
+    try testing.expectApproxEqAbs(2, static_result[1], tolerance);
 
-    const parallel_result = getMinDistSquared(pos_a, Vec2f{ 1, 0 }, pos_b, Vec2f{ 2, 0 });
-    try testing.expectApproxEqAbs(1, parallel_result, tolerance);
+    const parallel_result = solveMinDistSquared(pos_a, Vec2f{ 1, 0 }, pos_b, Vec2f{ 2, 0 });
+    try testing.expectApproxEqAbs(1, parallel_result[1], tolerance);
 
-    const opposite_result = getMinDistSquared(pos_a, Vec2f{ 1, 0 }, pos_b, Vec2f{ -1, 0 });
-    try testing.expectApproxEqAbs(1, opposite_result, tolerance);
+    const opposite_result = solveMinDistSquared(pos_a, Vec2f{ 1, 0 }, pos_b, Vec2f{ -1, 0 });
+    try testing.expectApproxEqAbs(1, opposite_result[1], tolerance);
 
-    const perpendicular_result = getMinDistSquared(pos_a, Vec2f{ 1, 0 }, pos_b, Vec2f{ 0, 1 });
-    try testing.expectApproxEqAbs(0, perpendicular_result, tolerance);
+    const perpendicular_result = solveMinDistSquared(pos_a, Vec2f{ 1, 0 }, pos_b, Vec2f{ 0, 1 });
+    try testing.expectApproxEqAbs(0, perpendicular_result[1], tolerance);
 }
 
 test "closest dist interval" {
     const pos_a = Vec2f{ 1, 0 };
     const pos_b = Vec2f{ 0, 1 };
 
-    const parallel_result = getMinDistSquaredForInterval(pos_a, Vec2f{ 1, 0 }, pos_b, Vec2f{ 2, 0 }, 0, 100);
-    try testing.expectApproxEqAbs(1, parallel_result, tolerance);
+    const parallel_result = solveMinDistSquaredClamp(pos_a, Vec2f{ 1, 0 }, pos_b, Vec2f{ 2, 0 }, 0, 100);
+    try testing.expectApproxEqAbs(1, parallel_result[1], tolerance);
 
-    const opposite_result = getMinDistSquaredForInterval(pos_a, Vec2f{ 1, 0 }, pos_b, Vec2f{ -1, 0 }, -1.5, -0.5);
-    try testing.expectApproxEqAbs(1, opposite_result, tolerance);
+    const opposite_result = solveMinDistSquaredClamp(pos_a, Vec2f{ 1, 0 }, pos_b, Vec2f{ -1, 0 }, -1.5, -0.5);
+    try testing.expectApproxEqAbs(1, opposite_result[1], tolerance);
 
-    const perpendicular_result = getMinDistSquaredForInterval(pos_a, Vec2f{ 1, 0 }, pos_b, Vec2f{ 0, 1 }, -3, 3);
-    try testing.expectApproxEqAbs(0, perpendicular_result, tolerance);
+    const perpendicular_result = solveMinDistSquaredClamp(pos_a, Vec2f{ 1, 0 }, pos_b, Vec2f{ 0, 1 }, -3, 3);
+    try testing.expectApproxEqAbs(0, perpendicular_result[1], tolerance);
 }
